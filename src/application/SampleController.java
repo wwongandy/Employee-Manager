1eb6493b7aa3fdf923c75b3bd141b0b1dbfa208e
Andy <aw087ok@gmail.com>
2019-09-28 23:28:41 +0100

    Added interface functions for UI event handling

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -2,4 +2,42 @@ package application;
 
 public class SampleController {
 	
+	// Employee form buttons handlers
+	public void clearAddFormFields() {
+
+	}
+	
+	// Employee profile traversal operations
+	public void viewEmployee() {
+		
+	}
+	
+	public void viewNextEmployee() {
+		
+	}
+	
+	public void viewPreviousEmployee() {
+		
+	}
+	
+	public void searchEmployee() {
+		
+	}
+	
+	// CRUD - JDBC to SQL Operations;
+	public void getAllEmployees() {
+		
+	}
+	
+	public void deleteEmployee() {
+		
+	}
+	
+	public void updateEmployee() {
+		
+	}
+	
+	public void addEmployee() {
+		
+	}
 }
