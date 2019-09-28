2e0274e0a77e7321803c6be5e3170369d1310672
Andy <aw087ok@gmail.com>
2019-09-29 00:04:58 +0100

    Population script now creates framework of table

--- populateTable.sql
+++ populateTable.sql
@@ -1,4 +1,12 @@
 -- Script for creating and populating the "Employees" table
 
-CREATE TABLE IF NOT EXISTS `employees` (   
-)
+CREATE TABLE IF NOT EXISTS `employees` (
+    `socialSecurityNumber` int(9) NOT NULL AUTO_INCREMENT,
+    `dateOfBirth` datetime NOT NULL,
+    `firstName` varchar(128) NOT NULL,
+    `surname` varchar(128) NOT NULL
+    -- `salary` varchar(128) NOT NULL,
+    -- `gender` varchar(128) NOT NULL
+
+    PRIMARY KEY (`socialSecurityNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
