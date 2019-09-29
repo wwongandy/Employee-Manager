99219df8f91acf9414e62a6847fd9593ae069907
Andy <aw087ok@gmail.com>
2019-09-29 17:05:12 +0100

    Table creation script fin.

--- populateTable.sql
+++ populateTable.sql
@@ -1,12 +1,15 @@
 -- Script for creating and populating the "Employees" table
 
+-- Creation
 CREATE TABLE IF NOT EXISTS `employees` (
     `socialSecurityNumber` int(9) NOT NULL AUTO_INCREMENT,
     `dateOfBirth` datetime NOT NULL,
     `firstName` varchar(128) NOT NULL,
-    `surname` varchar(128) NOT NULL
-    -- `salary` varchar(128) NOT NULL,
-    -- `gender` varchar(128) NOT NULL
+    `surname` varchar(128) NOT NULL,
+    `salary` float(11) NOT NULL,
+    `gender` char(1) NOT NULL,
 
     PRIMARY KEY (`socialSecurityNumber`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+-- Population TODO
