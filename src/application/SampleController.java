e8266483952c00ab7ee939499b562701989e9155
Andy <aw087ok@gmail.com>
2019-09-29 17:40:02 +0100

    Setup SQL connection configuration details in SampleController

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -2,6 +2,15 @@ package application;
 
 public class SampleController {
 	
+	// SQL database connection configuration
+	// Technically a security flaw, but thankfully our database is in localhost!
+	private final String SQL_USERNAME = "root";
+	private final String SQL_PASSWORD = "root";
+	private final String SERVER_NAME = "localhost";
+	private final int PORT_NUMBER = 3306;
+	private final String DATABASE_NAME = "test";
+	private final String TABLE_NAME = "employees";
+	
 	// Employee form buttons handlers
 	public void clearAddFormFields() {
 
